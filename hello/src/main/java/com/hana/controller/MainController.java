948bc1013c951735ed9658a7f973e97629f0f4da
ga0nii <kgw0124@kakao.com>
2024-03-26 09:26:54 +0900

    JSP를 이용한 Spring 구조

---
+++ hello/src/main/java/com/hana/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.hana.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+}
