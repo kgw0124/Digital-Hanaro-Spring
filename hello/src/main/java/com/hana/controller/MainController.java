25c426e984bbc63c25985876e977b19f4be5afac
ga0nii <kgw0124@kakao.com>
2024-03-26 15:54:58 +0900

    JSP 데이터 전달 적용

--- hello/src/main/java/com/hana/controller/MainController.java
+++ hello/src/main/java/com/hana/controller/MainController.java
@@ -3,13 +3,23 @@ package com.hana.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
 public class MainController {
     @RequestMapping("/")
-    public String main(){
+    public String main(Model m){ // Model의 역할 : 매개체(DB에서 빼온 데이터)를 담아서 JSP로 전달해준다.
+        List<String> list = new ArrayList<>();
+        list.add("DATA1");
+        list.add("DATA2");
+        list.add("DATA3");
+        m.addAttribute("data", "MyData"); // K-V 형태로 정보를 담는다.
+        m.addAttribute("dataList", list);
         return "index";
     }
 }
--- hello/src/main/webapp/views/index.jsp
+++ hello/src/main/webapp/views/index.jsp
@@ -1,9 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
 <h1>Hello</h1>
+<h2>${data}</h2>
+<c: forEach var="s" items="${datas}">
+    <h3>${s}</h3>
+</c:>
 </body>
 </html>
