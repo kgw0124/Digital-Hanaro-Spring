2e1511e6b4d7036ba6238af616ca005462396b35
ga0nii <kgw0124@kakao.com>
2024-03-28 13:17:27 +0900

    24.03.28

---
+++ sb2_admin/src/main/java/com/hana/sb2_admin/controller/CustController.java
@@ -0,0 +1,26 @@
+package com.hana.sb2_admin.controller;
+
+import com.hana.sb2_admin.app.data.dto.CustDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/cust")
+public class CustController {
+    String dir = "cust/";
+    @RequestMapping("/get")
+    public String get(Model model){
+        List<CustDto> list = new ArrayList<>();
+        list.add(new CustDto("id01", "pwd01", "james"));
+        list.add(new CustDto("id01", "pwd01", "james"));
+        list.add(new CustDto("id01", "pwd01", "james"));
+        list.add(new CustDto("id01", "pwd01", "james"));
+        model.addAttribute("custs", list);
+        model.addAttribute("center", dir+"get");
+        return "index";
+    }
+}
